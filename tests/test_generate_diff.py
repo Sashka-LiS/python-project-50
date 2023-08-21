304b6c686bbe544d7192e40ae70c0bf1a2385074
Alexandr Lisimenko <lisimenko.alexander@yandex.ru>
2023-08-21 12:08:27 +0300

    test commit

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,11 +2,10 @@ from gendiff.output_diff import generate_diff
 from gendiff.find_diff import find_difference
 
 
-path_file1 = "tests/fixtures/file1.json"
-path_file2 = "tests/fixtures/file2.json"
-diff = find_difference(path_file1, path_file2)
-reference = "{\n"+"  - follow: False"+"\n    host: hexlet.io"+"\n  - proxy: 123.234.53.22"+"\n  - timeout: 50"+"\n  + timeout: 20"+"\n  + verbose: True"+"\n}" # noqa
-
-
 def test_generate_diff():
-    assert generate_diff(diff) == reference
+    path_file1 = "tests/fixtures/file1.json"
+    path_file2 = "tests/fixtures/file2.json"
+    diff = find_difference(path_file1, path_file2)
+    with open("tests/fixtures/answer.txt") as answer_file:
+        result = "".join(answer_file.readlines())
+        assert generate_diff(diff) == result
