3dfec8a7e6bd4d299c61a6e08bb929a2ce2f8eef
Alexandr Lisimenko <lisimenko.alexander@yandex.ru>
2023-08-27 18:54:09 +0300

    fix plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,7 +1,7 @@
 from gendiff.parser import get_file_extension, parse
 
 
-def create_str(key, status, del_value = None, add_value = None):
+def create_str(key, status, del_value=None, add_value=None):
     if status == "removed":
         result_str = f"key: {key} / status: {status} / value: {del_value}\n"
     elif status == "added":
@@ -23,14 +23,14 @@ def plain(file1_path: str, file2_path: str) -> str:
     for key in all_keys:
         if key in data1 and key not in data2:
             status = "removed"
-            result_str += create_str(key, status, del_value = data1[key])
+            result_str += create_str(key, status, del_value=data1[key])
         elif key not in data1 and key in data2:
             status = "added"
-            result_str += create_str(key, status, add_value = data2[key])
+            result_str += create_str(key, status, add_value=data2[key])
         elif data1[key] == data2[key]:
             status = "unchanged"
-            result_str += create_str(key, status, add_value = data2[key])
+            result_str += create_str(key, status, add_value=data2[key])
         elif data1[key] != data2[key]:
             status = "changed"
-            result_str += create_str(key, status, del_value = data1[key], add_value = data2[key])
+            result_str += create_str(key, status, del_value=data1[key], add_value=data2[key])
     return result_str.rstrip()
