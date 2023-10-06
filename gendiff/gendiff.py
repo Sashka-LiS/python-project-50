9859ec16eee81f5fa14d0929d18a289d0fc07ca7
Alexandr Lisimenko <lisimenko.alexander@yandex.ru>
2023-10-06 11:41:51 +0300

    update gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,9 +1,10 @@
 from gendiff.parser import get_file_extension, parse
 from gendiff.find_diff import find_diff
 from gendiff.formatters.render import render
+from gendiff.constants import STYLISH
 
 
-def generate_diff(first_file: str, second_file: str, format: str) -> str:
+def generate_diff(first_file: str, second_file: str, format: str=STYLISH) -> str:
 
     data1 = parse(first_file, file_extension=get_file_extension(first_file))
     data2 = parse(second_file, file_extension=get_file_extension(second_file))
